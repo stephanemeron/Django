51f4e28b613199c315724d0c4ced21ba7acc6bc5
smeron@lesechos.fr <smeron@lesechos.fr>
2016-01-29 14:38:27 +0100

    Gestion views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,15 +7,22 @@ from django.views import generic
 from .models import Question, Choice, Content, Video
 from .forms import ContactForm
 
-# def index(request):
-# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# 	# template = loader.get_template('polls/index.html')
-# 	# context = RequestContext(request, {
-# 	# 	'latest_question_list': latest_question_list,
-# 	# })
-# 	# return HttpResponse(template.render(context))
-# 	context = {'latest_question_list': latest_question_list, 'error_message': "L'id n'existe pas, patttttttatte"}
-# 	return render(request, 'polls/index.html', context)
+def index(request):
+	# latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	arrayReturn = {};
+	"""Return the last five published questions."""
+	# return Question.objects.order_by('-pub_date')[:5]
+	arrayReturn['question'] = Question.objects.order_by('-pub_date')[:5]
+	arrayReturn['content'] = content(request)
+	arrayReturn['video'] = Video.objects.all()[:1]
+	arrayReturn['contact_form'] = contact_form(request)
+	# template = loader.get_template('polls/index.html')
+	# context = RequestContext(request, {
+	# 	'latest_question_list': latest_question_list,
+	# })
+	# return HttpResponse(template.render(context))
+	context = {'latest_question_list': arrayReturn, 'error_message': "L'id n'existe pas, patttttttatte"}
+	return render(request, 'polls/index.html', context)
 
 # def detail(request, question_id):
 # 	# Methode pour gerer le message erreur
@@ -34,18 +41,18 @@ from .forms import ContactForm
 # 	# response = "You're looking at the results of question %s."
 # 	return render(request, 'polls/results.html', {'question': question})
 
-class IndexView(generic.ListView):
+# class IndexView(generic.ListView):
 
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
-
-	def get_queryset(self):
-		arrayReturn = {};
-		"""Return the last five published questions."""
-		# return Question.objects.order_by('-pub_date')[:5]
-		arrayReturn['question'] = Question.objects.order_by('-pub_date')[:5]
-		arrayReturn['video'] = Video.objects.all()[:1]
-		return arrayReturn
+# 	template_name = 'polls/index.html'
+# 	context_object_name = 'latest_question_list'
+# 	def get_queryset(self):
+# 		arrayReturn = {};
+# 		"""Return the last five published questions."""
+# 		# return Question.objects.order_by('-pub_date')[:5]
+# 		arrayReturn['question'] = Question.objects.order_by('-pub_date')[:5]
+# 		arrayReturn['video'] = Video.objects.all()[:1]
+# 		arrayReturn['contact_form'] = contact_form(self.request)
+# 		return arrayReturn
 
 
 class DetailView(generic.DetailView):
@@ -73,19 +80,45 @@ def vote(request, question_id):
 		# user hits the Back button.
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-class ContentView(generic.ListView):
-	template_name = 'polls/content.html'
-	context_object_name = 'content_list'
+# class ContentView(generic.ListView):
+# 	template_name = 'polls/content.html'
+# 	context_object_name = 'content_list'
 
-	def get_queryset(self):
-		"""Return the list of content."""
-		return Content.objects.order_by('-created_at')
+# 	def get_queryset(self):
+# 		"""Return the list of content."""
+# 		return Content.objects.order_by('-created_at')
 
 class PageContentView(generic.DetailView):
 	model = Content
 	template_name = 'polls/page-content.html'
+	slug_field = 'slug'
 
+# def contact_form(request):
+# 		# if this is a POST request we need to process the form data
+# 		if request.method == 'POST':
+# 			# create a form instance and populate it with data from the request:
+# 			form = ContactForm(request.POST)
+# 			# check whether it's valid:
+# 			if form.is_valid():
+# 				contact = form.cleaned_data['contact_first_name']+' '+form.cleaned_data['contact_name']
+# 				subject = form.cleaned_data['contact_subject']
+# 				message = form.cleaned_data['contact_message']
+# 				sender = form.cleaned_data['contact_mail']
+# 				cc_myself = form.cleaned_data['cc_myself']
 
+# 				recipients = ['stephane.meron@gmail.com']
+# 				if cc_myself:
+# 					recipients.append(sender)
+
+# 				send_mail(subject, message, sender, recipients)
+# 				# redirect to a new URL:
+# 				return HttpResponseRedirect('/thanks/')
+
+# 		# if a GET (or any other method) we'll create a blank form
+# 		else:
+# 			form = ContactForm()
+
+# 		return render(request, 'polls/contact.html', {'form': form})
 def contact_form(request):
 	# if this is a POST request we need to process the form data
 	if request.method == 'POST':
@@ -110,5 +143,10 @@ def contact_form(request):
 	# if a GET (or any other method) we'll create a blank form
 	else:
 		form = ContactForm()
-
 	return render(request, 'polls/contact.html', {'form': form})
+
+def content(request):
+	content = Content.objects.order_by('-updated_at')[:5]
+# 	# Fin de la methode sans gerer le message d erreur, mais plus compacte
+# 	return render(request, 'polls/detail.html', {'question': question})
+	return render(request, 'polls/content.html', {'content_list': content})
