6b45a8b4aa6f69d94d131cfe2f063b5043a485d3
smeron@lesechos.fr <smeron@lesechos.fr>
2016-01-29 14:35:28 +0100

    url pour les différentes routes front et admin

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('polls.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
