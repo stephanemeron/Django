38679c4756cf0dea420736f70868973a2eac2213
smeron@lesechos.fr <smeron@lesechos.fr>
2016-01-29 14:37:35 +0100

    MAJ url pour polls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name='polls'
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    # url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
@@ -13,10 +14,11 @@ urlpatterns = [
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     
-    url(r'^contact/$', views.contact_form, name='contact'),
+    # url(r'^contact/$', views.contact_form, name='contact'),
     # ex: /polls/content
-    url(r'^content/$', views.ContentView.as_view(), name='content'),
-    # ex: /polls/1/content
-    url(r'^(?P<slug>[-\w]+)/$', views.PageContentView.as_view(), name='page-content'),
+    # url(r'^content/$', views.ContentView.as_view(), name='content'),
+    # ex: /polls/content/1/ r'^(?P<question_id>[0-9]+)/vote/$
+    url(r'^content/(?P<slug>[\w-]+)/$', views.PageContentView.as_view(), name='page-content'),
+    # url(r'^(?P<pk>[0-9]+)/content/$', views.PageContentView.as_view(), name='page-content'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve'),
 ]
